e54a2ed55125ad5331c004f3dd2c228040d98215
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-07-04 20:50:28 +0400

    disable postgreSQL container and rollback migration to sql

--- migrations/Version20240627073402.php
+++ migrations/Version20240627073402.php
@@ -7,6 +7,9 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
 final class Version20240627073402 extends AbstractMigration
 {
     public function getDescription(): string
@@ -17,16 +20,15 @@ final class Version20240627073402 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE director (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, nationality VARCHAR(30) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE movie (id SERIAL NOT NULL, director_id INT NOT NULL, title VARCHAR(100) NOT NULL, note DOUBLE PRECISION NOT NULL, release_date DATE NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_1D5EF26F899FB366 ON movie (director_id)');
-        $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1D5EF26F899FB366 FOREIGN KEY (director_id) REFERENCES director (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE TABLE director (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, nationality VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, director_id INT NOT NULL, title VARCHAR(100) NOT NULL, note DOUBLE PRECISION NOT NULL, release_date DATE NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_1D5EF26F899FB366 (director_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1D5EF26F899FB366 FOREIGN KEY (director_id) REFERENCES director (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE movie DROP CONSTRAINT FK_1D5EF26F899FB366');
+        $this->addSql('ALTER TABLE movie DROP FOREIGN KEY FK_1D5EF26F899FB366');
         $this->addSql('DROP TABLE director');
         $this->addSql('DROP TABLE movie');
     }
